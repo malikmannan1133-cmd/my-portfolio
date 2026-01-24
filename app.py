634e51d58c91b366cf358411bf17a31a530667ca
Malik Mannan <malikmannan1133@gmail.com>
2026-01-24 22:59:37 +0500

    Final Vercel config with app.py

--- app.py
+++ app.py
@@ -6,27 +6,31 @@ app = Flask(__name__)
 projects_data = [
     {
         "title": "E-commerce Sales Analytics",
-        "tech": "Python(NumPy,Pandas), MySQL, Power BI",
-        "description": "Comprehensive analysis of $7.74M revenue data to identify growth drivers. Developed interactive dashboards to track monthly sales trends.",
-        "image_file": "ecomerce.png"
+        "tech": "Python, MySQL, Power BI",
+        "description": "Comprehensive analysis of $7.74M revenue data to identify growth drivers.",
+        "image_file": "ecomerce.png",
+        "video_file": "ecomerce.mp4"
     },
     {
         "title": "HR Employee Analytics",
-        "tech": "Power BI,Python(NumPy,Pandas), MySql",
-        "description": "Data-driven approach to workforce management. Analyzed employee performance and retention metrics to identify turnover risks.",
-        "image_file": "hranalystics.png"
+        "tech": "Power BI, Python, MySQL",
+        "description": "Data-driven approach to workforce management and retention metrics.",
+        "image_file": "hranalystics.png",
+        "video_file": "hranalytics.mp4"
     },
     {
         "title": "Pizza Shop Sales Analysis",
         "tech": "MySQL, Power BI",
-        "description": "End-to-end SQL project to track daily sales, customer preferences, and inventory turnover. Optimized order processing insights.",
-        "image_file": "pizzasalesproject.png"
+        "description": "End-to-end SQL project to track daily sales and customer preferences.",
+        "image_file": "pizzasalesproject.png",
+        "video_file": "pizzasales.mp4"
     },
     {
         "title": "Medical Whole Sale Analysis",
-        "tech": "MySQL,Python(NumPy,Pandas),Power BI",
-        "description": "Inventory and supply chain analysis for a medical wholesaler. Used SQL and Python to track stock levels and expiry dates.",
-        "image_file": "medicalinventory.png"
+        "tech": "MySQL, Python, Power BI",
+        "description": "Inventory and supply chain analysis for a medical wholesaler.",
+        "image_file": "medicalinventory.png",
+        "video_file": "media project.mp4"
     }
 ]
 
@@ -34,7 +38,6 @@ projects_data = [
 def home():
     return render_template('index.html', projects=projects_data)
 
-# Vercel ke liye ye line zaroori hai
 app = app
 
 if __name__ == '__main__':
